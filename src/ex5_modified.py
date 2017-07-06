55552dad02b316e99f8e41e41a3d1ee2b113d3b2
anil.bharadia <anil.bharadia@gmail.com>
2017-07-06 23:02:34 +0530

    completed ex6

---
+++ src/ex5_modified.py
@@ -0,0 +1,21 @@
+name = "Anil D. Bharadia"
+age = 35
+height = 74 # inches
+weight = 180 # pounds
+eyes = "Black"
+teeth = "White"
+hair = "Black"
+
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "That is %f centimeters" % (2.54 * height)
+print "He's %d pounds heavy." % weight
+print "That is %f kilograms." % (0.453592 * weight)
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are %s depending on the coffee." % teeth
+
+
+print "If I add %d, %d and %d I get %d" %(age, height, weight, age + height + weight)
+
+print "round() of 1.7333 is %d" % round(1.7333)
